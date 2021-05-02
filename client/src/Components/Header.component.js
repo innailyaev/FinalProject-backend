8397ac2b3a093c1bf8d26632c0307d7a640759f0
Inna Ilyaev <inna4ilyaev@gmail.com>
2021-05-02 15:50:15 +0300

    02/05/21#1

---
+++ client/src/Components/Header.component.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+import '../CSS/Header.css';
+
+const Header = () => {
+  return (
+    <div className="header">
+
+      <div className="headerContainer">
+        <div>
+        <Link><div className="logo"></div></Link>
+          <Link to="/">
+           HOMEPAGE
+          </Link>
+        </div>
+        <div />
+        <Link to="/login">LogIn</Link>
+      </div>
+
+    </div>
+  );
+};
+
+export default Header;
---
+++ client/src/Components/SignUp.jsx
@@ -0,0 +1,56 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+const SignApp =()=>{
+
+    const [name,setName] = useState('');
+    const [email,setEmail] = useState('');
+    const [password,setPassword] = useState('');
+
+    const postNewUser= async () => {
+        console.log("post");
+        try{
+           const response = await axios.post('https://finalprojectinna.herokuapp.com/api/users/signup', {
+                name:name,
+                email:email,
+                password:password
+        });
+            console.log(response);
+            // if(response.data.error){
+            //     setError(response.data.error);
+            // }
+            // else{
+            //     setError('');
+            // }
+           
+        }catch(err){
+                console.log(err); 
+        }
+    }
+
+    const formHandler = (e)=>{
+        e.preventDefault()
+    }
+
+    const clickHandler = ()=>{
+        postNewUser();
+    }
+
+  return (
+    <div className="signUpContainer">
+        <div className="formDiv">
+            <form onSubmit={formHandler} className="formContainer">
+                <label>Name</label>
+                <input type="text" onChange={(e)=>setName(e.target.value)}/>
+                <label>Email</label>
+                <input type="text" onChange={(e)=>setEmail(e.target.value)}/>
+                <label>Password</label>
+                <input type="text" onChange={(e)=>setPassword(e.target.value)}/>
+                <button onClick={clickHandler} className="btn">Submit</button>
+            </form>
+        </div>
+    </div>
+  );
+}
+
+export default SignApp;
